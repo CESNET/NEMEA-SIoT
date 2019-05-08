24e7e2ad842837a1d8d1773682e47dce8fa7492f
Martin Hošala <xhosal00@stud.fit.vutbr.cz>
2019-05-08 10:57:17 +0200

    Needs to be tried on Turris
    Signed-off-by: Martin Hošala <xhosal00@stud.fit.vutbr.cz>

--- IntegrationTest.py
+++ IntegrationTest.py
@@ -16,35 +16,41 @@ from TestUtil.Shell import Shell
 class IntegrationTest(Namespace):
     def __init__(self):
         super().__init__()
-        self.__turris = True
-        self.__turris_meta = {'config': Path("/etc/config/nemea-supervisor")}
-        # self.__turris_meta = {'config': Path(
-        #     "/home/xhosal00/school/bp/impl/siot-nemea/TestUtil/nemea-supervisor.makak.conf")}
+        self.__t = True
+        # self.__t_meta = {'config': Path("/etc/config/nemea-supervisor")}
+        self.__t_meta = {'config': Path("./TestUtil/turris-supervisorl-test.conf")}
 
-    def __turris_backup_sv_conf__(self):
-        sv_conf = self.__turris_meta['config']
+    def __t_backup_sv_conf__(self):
+        sv_conf = self.__t_meta['config']
         if sv_conf.is_file():
-            sv_conf_backup = '{}/{}.backup'.format(os.getcwd(), sv_conf.name)
+            sv_conf_backup = Path('{}/{}.backup'.format(os.getcwd(), sv_conf.name))
             copy2(sv_conf, sv_conf_backup)
-            self.__turris_meta['config-backup'] = sv_conf_backup
+            self.__t_meta['config-backup'] = sv_conf_backup
 
-    def __turris_restore_sv_conf__(self):
-        if self.__turris_meta['config-backup'] is not None:
-            copy2(self.__turris_meta['config-backup'], self.__turris_meta['config'])
-            os.remove(self.__turris_meta['config-backup'])
+    def __t_restore_sv_conf__(self):
+        if self.__t_meta['config-backup'] is not None:
+            self.__t_cp_to_sv_conf_loc__(self.__t_meta['config-backup'])
+            os.remove(self.__t_meta['config-backup'])
 
-    def __test_on_turris__(self):
+    def __t_cp_to_sv_conf_loc__(self, path: Path):
+        copy2(path, self.__t_meta['config'])
+
+    def __test_on_t__(self):
         supervisor = Path("/etc/init.d/nemea-supervisorl")
         if not supervisor.is_file():
             pass
             # return
 
-        self.__turris_backup_sv_conf__()
-        self.__turris_restore_sv_conf__()
+        self.__t_backup_sv_conf__()
+        self.__t_cp_to_sv_conf_loc__(self.__t_meta['test-config'])
+
+        input("Press Enter to continue...")
+
+        self.__t_restore_sv_conf__()
 
     def run(self):
-        if self.__turris:
-            self.__test_on_turris__()
+        if self.__t:
+            self.__test_on_t__()
 
 
 
