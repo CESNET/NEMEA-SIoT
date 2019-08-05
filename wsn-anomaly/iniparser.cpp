7d97987c53ee0d7146696c7e8758f79d4ae78157
Dominik Soukup <soukupdo@gmail.com>
2019-08-05 21:40:16 +0200

    Iterate over sections

--- wsn-anomaly/iniparser.cpp
+++ wsn-anomaly/iniparser.cpp
@@ -5,16 +5,52 @@
 using namespace std;
 
 int main (){
-INIReader reader("config.txt");
+INIReader reader("test.ini");
+
 if (reader.ParseError() < 0){
-    cout << "can not load test file";
+    cout << "Can not load test file";
     return 1;
 }
 
 cout << "config file loaded" << endl;
 
-cout << reader.Get("SOFCount","profile","UNK") << endl;
+set<string> sections = reader.Sections();
+
+for (auto it=sections.begin(); it != sections.end(); ++it){ 
+        cout << ' ' << *it; 
+        cout << reader.GetInteger(*it,"id",-1) << endl;
+        cout << reader.GetInteger(*it,"len",-1) << endl;
+        cout << reader.GetInteger(*it,"learn",-1) << endl;
+        cout << reader.GetInteger(*it,"ignore",-1) << endl;
+        cout << reader.Get(*it,"store","None") << endl;
+        cout << reader.GetInteger(*it,"check",-1) << endl;
+        cout << reader.GetInteger(*it,"export",-1) << endl;
+}
+
+//cout << reader.GetInteger("SOFCount","profile","UNK") << endl;
+
+
+//reader.Sections();
 
 
 return 1;
 }
+
+/*
+td::string Get(std::string section, std::string name,
+335                     std::string default_value) const;
+336
+337     // Get an integer (long) value from INI file, returning default_value if
+338     // not found or not a valid integer (decimal "1234", "-1234", or hex "0x4d2").
+339     long GetInteger(std::string section, std::string name, long default_value) const;
+340
+341     // Get a real (floating point double) value from INI file, returning
+342     // default_value if not found or not a valid floating point value
+343     // according to strtod().
+344     double GetReal(std::string section, std::string name, double default_value) const;
+345
+346     // Get a boolean value from INI file, returning default_value if not found or if
+347     // not a valid true/false value. Valid true values are "true", "yes", "on", "1",
+348     // and valid false values are "false", "no", "off", "0" (not case sensitive).
+349     bool GetBoolean(s
+*/
