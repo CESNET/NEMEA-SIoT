c228ddb03832c7643d04aa3824feef7f4f02ecb2
Ondřej Hujňák <hujnak@cesnet.cz>
2019-08-13 09:36:07 +0200

    BLEAdvScanner: Initial test implementation of NEMEA interfaces.

---
+++ ble-adv-scanner/BLEAdvScanner.cpp
@@ -0,0 +1,15 @@
+#include <csignal>
+#include <errno.h>
+#include <getopt.h>
+#include <libtrap/trap.h>
+#include <unirec/unirec.h>
+#include <unistd.h>
+
+#include "fields.h"
+
+int main(int argc, char **argv)
+{
+	int ret = 0;
+
+	return ret;
+}
