1542332bf4a46d338597e750f281991f0467bf0c
Martin Hošala <xhosal00@stud.fit.vutbr.cz>
2019-08-15 10:58:35 +0200

    valgrind for tests

--- AutoTest.py
+++ AutoTest.py
@@ -2,7 +2,7 @@ from TestUtil.SIoTNemeaTester import SIoTNemeaTester
 from TestUtil.Shell import Shell
 from argparse import ArgumentParser
 import json
-import subprocess
+from subprocess import Popen, PIPE
 
 
 class AutoTest(SIoTNemeaTester):
@@ -11,8 +11,14 @@ class AutoTest(SIoTNemeaTester):
         return './siot-{0}'.format(m)
 
     def run(self) -> int:
-        self.__prepare_test_output_directories__(self.repo.modules)
+        valgrind = []
+        if self.v:
+            valgrind = ['valgrind', '--error-exitcode=42']
 
+        if self.apvo and not self.v:
+            self.parser.error('apvo agrument is dependand on v argument')
+
+        self.__prepare_test_output_directories__(self.repo.modules)
         for m in self.repo.modules:
             if not self.repo.got_tests(m):
                 continue
@@ -50,23 +56,27 @@ class AutoTest(SIoTNemeaTester):
 
                 # starts the module with appropriate arguments for testing plus
                 # with potential arguments defined in the auto-tests.json file
-                module = subprocess.Popen(
-                    [self.__get_expected_executable__(m),
+                module = Popen(
+                     valgrind + [self.__get_expected_executable__(m),
                      '-i', '{},{}'.format(ifc_in, ifc_out)] + run_args,
-                     stderr=subprocess.PIPE, stdout=subprocess.PIPE, cwd=m)
+                     stderr=PIPE, stdout=PIPE, cwd=m)
 
                 self.__inject_and_capture__(ifc_in, f_in, ifc_out, f_out)
                 x = Shell(directory=m)
                 x.execute_array(post_shell)
 
                 self.__test_ended__(self.__compare_files__(f_exp, f_out))
+                out, err = module.communicate()
+                if module.returncode == 42 or (self.v and self.apvo):
+                    print(err.decode())
 
         return self.__finished__()
 
 
 def main():
     parser = ArgumentParser(description='Auto testing for NEMEA-SIoT.')
-
+    parser.add_argument('-v', action='store_true', help='test with valgrind')
+    parser.add_argument('--apvo', action='store_true', help='always print valgrind output')
     auto_test = AutoTest(parser)
     exit(auto_test.run())
 
