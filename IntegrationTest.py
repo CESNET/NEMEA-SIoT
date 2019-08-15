ec0ccb8054ff97cbd89172f52e3618e5eb3ea3f1
Martin Hošala <xhosal00@stud.fit.vutbr.cz>
2019-08-15 12:44:52 +0200

    IntegrationTest: fix missong os, wrong super call
    Signed-off-by: Martin Hošala <xhosal00@stud.fit.vutbr.cz>

--- IntegrationTest.py
+++ IntegrationTest.py
@@ -1,15 +1,20 @@
 from TestUtil.SIoTNemeaTester import SIoTNemeaTester
 from TestUtil.Shell import Shell
 from argparse import ArgumentParser
-import json
+import os
 import subprocess
 from shutil import copy2
 from pathlib import Path
 
+
 class IntegrationTest(SIoTNemeaTester):
     def __init__(self, arg: ArgumentParser):
-        super.__init__(arg)   
+        super().__init__(arg)
         self.p = False
+        self.__t_meta = {
+            'config': Path("/etc/config/nemea-supervisor"),
+            'test-config': Path("./TestUtil/turris-supervisorl-test.conf")
+        }
 
     # This method backups NEMEA SupervisorL configuration which is already
     # present at the router
@@ -33,60 +38,60 @@ class IntegrationTest(SIoTNemeaTester):
             print('hm')
 
         copy2(path, self.__t_meta['config'])
-        
+
     def __clean_action__(self):
-            self.__t_restore_sv_conf__()
-    
+        self.__t_restore_sv_conf__()
+
     def run(self) -> int:
         self.__prepare_test_output_directories__(self.repo.modules)
         supervisor = Path("/etc/init.d/nemea-supervisorl")
         if not supervisor.is_file():
             raise RuntimeError('NEMEA SupervisorL not found: {} is not a file'.format(supervisor))
-            
+
         self.__t_backup_sv_conf__()
         self.__t_cp_to_sv_conf_loc__(self.__t_meta['test-config'])
         subprocess.Popen([supervisor, 'reload'])
         subprocess.Popen([supervisor, 'start'])
-        
+
         if self.p and not self.quit:
             input("You can now verify whether the modules are running - Press Enter to continue...")
-        
+
         for m in self.repo.modules:
             if not self.repo.got_tests(m):
                 continue
-            
+
             tests = self.repo.get_tests(m)
-            
+
             if m == 'ble-pairing':
                 x = Shell()
                 x.execute('mkdir /tmp/ble-pairing-test/')
-            
+
             for t in tests:
                 if self.quit:
                     return 2
-                
+
                 if m == 'ble-pairing':
                     x = Shell()
                     x.execute('rm -rf /tmp/ble-pairing-test/*')
-                
+
                 self.__test_started__(m, t)
-                
+
                 f_in = self.__get_test_in_path__(m, t)
                 f_out = self.__get_test_out_path__(m, t)
                 f_exp = self.__get_test_expected_out_path__(m, t)
-                ifc_in = 'u:test-in'
-                ifc_out = 'u:test-out'
-                                        
+                ifc_in = 'u:tin-{}'.format(m)
+                ifc_out = 'u:test-{}'.format(m)
+
                 self.__inject_and_capture__(ifc_in, f_in, ifc_out, f_out)
                 self.__test_ended__(self.__compare_files__(f_exp, f_out))
-            
+
             if m == 'ble-pairing':
                 x = Shell()
                 x.execute('rm -rf /tmp/ble-pairing-test/')
-            
+
         return self.__finished__()
-        
-        
+
+
 def main():
     parser = ArgumentParser(description='Auto testing for NEMEA-SIoT.')
     parser.add_argument('-p', action='store_true', help='Pauses the script before and after the test so that it is possible to verify wether modules are correctly running')
