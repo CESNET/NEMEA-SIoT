3f24e528560e5296cf44ff6def7d6fd1d0b79b5b
Dominik Soukup <soukupdo@gmail.com>
2019-12-24 15:16:17 +0100

    Modified UniRec field based on unified format

--- lora-distance/fields.c
+++ lora-distance/fields.c
@@ -4,27 +4,36 @@
 
 char *ur_field_names_static[] = {
    "BASE_RSSI",
+   "INCIDENT_DEV_ADDR",
    "RSSI",
    "TIMESTAMP",
    "VARIANCE",
+   "ALERT_CODE",
+   "CAPTION",
    "DEV_ADDR",
    "PHY_PAYLOAD",
 };
 short ur_field_sizes_static[] = {
    8, /* BASE_RSSI */
+   8, /* INCIDENT_DEV_ADDR */
    8, /* RSSI */
    8, /* TIMESTAMP */
    8, /* VARIANCE */
+   4, /* ALERT_CODE */
+   -1, /* CAPTION */
    -1, /* DEV_ADDR */
    -1, /* PHY_PAYLOAD */
 };
 ur_field_type_t ur_field_types_static[] = {
    UR_TYPE_DOUBLE, /* BASE_RSSI */
+   UR_TYPE_UINT64, /* INCIDENT_DEV_ADDR */
    UR_TYPE_DOUBLE, /* RSSI */
-   UR_TYPE_TIME, /* TIMESTAMP */
+   UR_TYPE_UINT64, /* TIMESTAMP */
    UR_TYPE_DOUBLE, /* VARIANCE */
+   UR_TYPE_UINT32, /* ALERT_CODE */
+   UR_TYPE_STRING, /* CAPTION */
    UR_TYPE_STRING, /* DEV_ADDR */
    UR_TYPE_STRING, /* PHY_PAYLOAD */
 };
-ur_static_field_specs_t UR_FIELD_SPECS_STATIC = {ur_field_names_static, ur_field_sizes_static, ur_field_types_static, 6};
-ur_field_specs_t ur_field_specs = {ur_field_names_static, ur_field_sizes_static, ur_field_types_static, 6, 6, 6, NULL, UR_UNINITIALIZED};
+ur_static_field_specs_t UR_FIELD_SPECS_STATIC = {ur_field_names_static, ur_field_sizes_static, ur_field_types_static, 9};
+ur_field_specs_t ur_field_specs = {ur_field_names_static, ur_field_sizes_static, ur_field_types_static, 9, 9, 9, NULL, UR_UNINITIALIZED};
--- lora-distance/fields.h
+++ lora-distance/fields.h
@@ -6,15 +6,21 @@
 
 #define F_BASE_RSSI   0
 #define F_BASE_RSSI_T   double
-#define F_RSSI   1
+#define F_INCIDENT_DEV_ADDR   1
+#define F_INCIDENT_DEV_ADDR_T   uint64_t
+#define F_RSSI   2
 #define F_RSSI_T   double
-#define F_TIMESTAMP   2
-#define F_TIMESTAMP_T   ur_time_t
-#define F_VARIANCE   3
+#define F_TIMESTAMP   3
+#define F_TIMESTAMP_T   uint64_t
+#define F_VARIANCE   4
 #define F_VARIANCE_T   double
-#define F_DEV_ADDR   4
+#define F_ALERT_CODE   5
+#define F_ALERT_CODE_T   uint32_t
+#define F_CAPTION   6
+#define F_CAPTION_T   char
+#define F_DEV_ADDR   7
 #define F_DEV_ADDR_T   char
-#define F_PHY_PAYLOAD   5
+#define F_PHY_PAYLOAD   8
 #define F_PHY_PAYLOAD_T   char
 
 extern uint16_t ur_last_id;
